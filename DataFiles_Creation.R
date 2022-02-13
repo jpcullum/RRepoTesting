0e0fd07202c1de7216d2836bc15ca1db74c4cf20
jpcullum <joshuapcullum@gmail.com>
2022-02-12 19:39:02 -0600

    Further design ideas

--- DataFiles_Creation.R
+++ DataFiles_Creation.R
@@ -12,3 +12,18 @@ library(tidyverse)
 #it should incorporate multiple variables that relate, and should hold a temporal aspect
 #we should be able to run PCA tests off of it as well
 
+#Columns:
+#unique sample identifier
+#data
+#value of several variables
+
+#we may also want to play with built in datafiles, such as mtcars
+#the problem there is I do not want to replicate work done by others
+#this is to show my work, and I cannot copy and paste code for a
+#datafile that I created myself
+
+#let us work with something we are familiar with
+#for example, let us work with well data
+#here, we will have GPS data, depth data, and then values of certain fake elements tested
+#we should create patterns based on location, depth and spatially
+
