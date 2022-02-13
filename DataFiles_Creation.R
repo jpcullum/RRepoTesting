4a0c0b46149e60b1f893714652b0b0cd037fa287
jpcullum <joshuapcullum@gmail.com>
2022-02-12 19:32:37 -0600

    Creating our data files

---
+++ DataFiles_Creation.R
@@ -0,0 +1,14 @@
+#Creating a datafile for analysis
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(tidyverse)
+
+
+#here we want to design and create a datafile that simulates natural patterns
+#then, we can investigate visually, and then create an ML model off of
+#this should be large, large enough to split into building and testing phases
+#it should incorporate multiple variables that relate, and should hold a temporal aspect
+#we should be able to run PCA tests off of it as well
+
